20c1169cbadb1690f2977286078b66b2b7d002f5
quythanh <quythanh.dev@gmail.com>
2024-07-31 19:14:39 +0700

    fix: add getter, setter; change id type to String

--- app/src/main/java/com/example/movie_ticket_booking/Models/BaseModel.java
+++ app/src/main/java/com/example/movie_ticket_booking/Models/BaseModel.java
@@ -1,9 +1,16 @@
 package com.example.movie_ticket_booking.Models;
 
+import com.example.movie_ticket_booking.Interfaces.Identifiable;
+
 import java.util.Date;
 
-public abstract class BaseModel {
-    protected int id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public abstract class BaseModel implements Identifiable {
+    protected String id;
     protected Date createdDate;
     protected boolean isActive;
 }
