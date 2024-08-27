38aebc7e4e57c7f68a2e40822c5b22cbc55bae20
quythanh <quythanh.dev@gmail.com>
2024-08-27 13:36:59 +0700

    fix: add missing field `description` and constructor, getter, setter for Product

--- app/src/main/java/com/example/movie_ticket_booking/Models/Product.java
+++ app/src/main/java/com/example/movie_ticket_booking/Models/Product.java
@@ -2,9 +2,19 @@ package com.example.movie_ticket_booking.Models;
 
 import androidx.annotation.NonNull;
 
-public class Product extends BaseModel{
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product extends BaseModel {
     private String name;
     private int unitPrice;
+    private String description;
 
     @NonNull
     @Override
