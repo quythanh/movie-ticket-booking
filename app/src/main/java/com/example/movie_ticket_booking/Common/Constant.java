ff8efb58683fa422e2ecf981710ea1e37ff2620c
quythanh <quythanh.dev@gmail.com>
2024-08-30 08:46:54 +0700

    feat: add Time constant

--- app/src/main/java/com/example/movie_ticket_booking/Common/Constant.java
+++ app/src/main/java/com/example/movie_ticket_booking/Common/Constant.java
@@ -11,6 +11,16 @@ public class Constant {
     public static final SimpleDateFormat TIME_FORMATTER = new SimpleDateFormat("HH:mm");
     public static final NumberFormat currencyFormatter = NumberFormat.getCurrencyInstance(new Locale("vn", "VN"));
 
+    public static final class Time {
+        public static final long SEC = 1000;
+        public static final long MINUTE = 60 * SEC;
+        public static final long HOUR = 60 * MINUTE;
+        public static final long DAY = 24 * HOUR;
+        public static final long WEEK = 7 * DAY;
+        public static final long MONTH = 30 * DAY;
+        public static final long QUARTER = 3 * MONTH;
+    }
+
     static {
         currencyFormatter.setCurrency(Currency.getInstance("VND"));
     }
--- app/src/main/java/com/example/movie_ticket_booking/Controllers/ShowtimeController.java
+++ app/src/main/java/com/example/movie_ticket_booking/Controllers/ShowtimeController.java
@@ -36,7 +36,7 @@ public class ShowtimeController extends GenericController<Showtime> {
         Date startDate = Constant.DATETIME_FORMATTER.parse(Constant.DATE_FORMATTER.format(date) + " 00:00");
         Date endDate = (Date) startDate.clone();
         Date now = new Date();
-        endDate.setTime(startDate.getTime() + 1000*60*60*24);
+        endDate.setTime(startDate.getTime() + Constant.Time.DAY);
 
         Timestamp start = new Timestamp(startDate.before(now) ? now : startDate);
         Timestamp end = new Timestamp(endDate);
